b1fec1b73bf5e584a14c40d191193700b554f778
LordDregg <oluwatosinodejide121@gmail.com>
2026-02-26 18:57:01 +0100

    little comment edit

--- contracts/Week 6/MultiSig.sol
+++ contracts/Week 6/MultiSig.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.3;
 contract MultiSigWallet {
 //   =====================================================================================
 //   |                                                                                   |
-//   |  1. At Deployment, all 3 Signers addresses will be provided                         |
+//   |  1. At Deployment, all 3 Signers addresses will be provided                       |
 //   |                                                                                   |
 //   |  2. Any signer can Create Product to be sold on the platform                      |
 //   |                                                                                   |
@@ -234,7 +234,7 @@ contract MultiSigWallet {
 
 //   =====================================================================================
 //   |                                                                                   |
-//   |  1. At Deployment, all 3 Signers addresses will be provided                         |
+//   |  1. At Deployment, all 3 Signers addresses will be provided                       |
 //   |                                                                                   |
 //   |  2. Any signer can Create Product to be sold on the platform                      |
 //   |                                                                                   |
@@ -243,5 +243,3 @@ contract MultiSigWallet {
 //   |  4. (2/3) signers must approve before a Withdrawal can be made                    |
 //   |                                                                                   |
 //   =====================================================================================
-
-// 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4, 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2, 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db, 2
